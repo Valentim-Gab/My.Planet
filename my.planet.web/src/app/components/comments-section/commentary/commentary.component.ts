11cc55e4542719e2d836091369e24ab7c7a548ff
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2023-03-31 12:24:54 -0300

    Finished comment functionality #7

--- my.planet.web/src/app/components/comments-section/commentary/commentary.component.ts
+++ my.planet.web/src/app/components/comments-section/commentary/commentary.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Commentary } from 'src/app/interfaces/Commentary';
 import { User } from 'src/app/interfaces/User';
+import { JwtService } from 'src/app/security/jwt.service';
 import { CommentaryService } from 'src/app/services/commentary.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,11 +13,18 @@ import { UserService } from 'src/app/services/user.service';
 export class CommentaryComponent implements OnInit {
   @Input() commentary!: Commentary
   @Output() emitterDeleteCommentary: EventEmitter<Commentary> = new EventEmitter<Commentary>()
+  deleteComentaryOptions: boolean = false
+  showDeleteIcon: boolean = false
 
-  constructor() { }
+  constructor(private jwtService: JwtService) { }
 
   ngOnInit(): void {
-  
+    if (this.commentary.user?.id == this.jwtService.getTokenSub())
+      this.showDeleteIcon = true
+  }
+
+  showDeleteComentaryOptions() {
+    this.deleteComentaryOptions = !this.deleteComentaryOptions
   }
 
   deleteCommenatary() {
